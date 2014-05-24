62cd5e799174914b866087aa8b962164e9de446d
unndf <phenanimao@gmail.com>
2014-05-24 01:39:57 -0700

    .

--- alist.tcl
+++ alist.tcl
@@ -9,64 +9,78 @@ namespace eval db_handler {
         list_db eval {CREATE TABLE anime( anime_title text,
                                    no_eps int,
                                    no_eps_watched int,
-                                   eps_titles text,
                                    descr text,
                                    rating text,
-                                   type text,
                                    status text,
                                    notes text
                                    )}
     }
-    proc mark_completed {id rating notes} {
-    }
-    proc add_backlog {id eps_no notes} {
-    }
-    proc remove_backlog {id} {
-    }
-    proc add_anime {id anime_title no_eps eps_title descr notes} {
-    }
     proc remove_anime {id} {
+        list_db eval {DELETE FROM anime WHERE rowid == id}
     }
     proc cleanup {} {
         list_db close
     }
     proc search_db {phrase} {
+        return [list_db eval {SELECT anime_title FROM anime WHERE anime_title LIKE "%$phrase%"}]
     }
     proc get_completed {} {
+        return [list_db eval {SELECT * FROM anime WHERE no_eps_watched == no_eps}]
     }
     proc get_backlog {} {
+        return [list_db eval {SELECT * FROM anime WHERE no_eps_watched == 0}]
     }
     proc get_anime_list {} {
         return [list_db eval {SELECT * FROM anime}]
     }
-    proc add_title {title no_eps no_eps_watched eps_titles descr rating type status notes} {
-        list_db eval {INSERT INTO anime (anime_title, no_eps, no_eps_watched, eps_titles, descr, rating, type, status, notes)
-                             VALUES($title, $no_eps, $no_eps_watched, $eps_titles, $desr, $rating,$type, $status, $notes)
+    proc add_title {title no_eps no_eps_watched descr rating status notes} {
+        list_db eval {INSERT INTO anime (anime_title, no_eps, no_eps_watched, descr, rating, status, notes)
+                             VALUES($title, $no_eps, $no_eps_watched, $desr, $rating, $status, $notes)
                      }
     }
-
 }
+#TODO: Actually make the gui look good
 namespace eval alist_gui {
+    namespace eval event_handlers {
+        proc add_dialog {title noeps descr status notes} {
+           ::db_handler::add_title $title $noeps 0 $descr 0 $status $notes
+        }
+    }
     proc add_dialog {} { 
         #ensure that only a single add_window is open at a time
         if [expr ![catch {toplevel .add_window } ] ] {
             wm title .add_window "Add Title to Database"
-            ttk::button .add_window.submit -text "Submit"
             ttk::label .add_window.titlelab -text "Title"
             ttk::entry .add_window.titleen
             ttk::label .add_window.no_epslab -text "No. Episodes"
             ttk::entry .add_window.no_epsen
-            ttk::label .add_window.eps_titleslab -text "Episode Titles"
-            ttk::entry .add_window.eps_titlesen
-            ttk::label .add_window.desclab -text "Title"
-            ttk::text .add_window.descrbox
-            ttk::label .add_window.typelab -text "Type"
-            ttk::listbox .add_window.typebox
+            ttk::label .add_window.descrlab -text "Description" -textvariable descr
+            text .add_window.descrbox
+            ttk::label .add_window.statuslab -text "Status" 
+            listbox .add_window.statusbox 
+            .add_window.statusbox insert 0 Watching Backlogged "Plan to Watch"
 
-            grid .add_window.submit -row 2 -column 0
+            ttk::label .add_window.noteslab -text "Description"
+            text .add_window.notesbox 
+
+            ttk::button .add_window.submit -text "Submit" 
+            
+            grid .add_window.submit -row 4 -column 0
             grid .add_window.titlelab -row 0 -column 0
             grid .add_window.titleen -row 0 -column 1
+            grid .add_window.no_epslab -row 1 -column 0
+            grid .add_window.no_epsen -row 1 -column 1
+            grid .add_window.descrlab -row 2 -column 0
+            grid .add_window.descrbox -row 2 -column 1
+            grid .add_window.statuslab -row 3 -column 0
+            grid .add_window.statusbox -row 3 -column 1
         }
+    }
+    proc populate_list {} {
+        #populates the treeview "list" in the GUI
+        #test
+        #.tabs.collection.rightpane.list insert {} end -id serie  -values {asas asas asasa asa asas}
+        #.tabs.collection.rightpane.list insert {} end -id series -text "asasa"
     }
     proc start {} { 
 #--------Create GUI components---------
@@ -86,23 +100,21 @@ namespace eval alist_gui {
 #--------Fill frames with Widgets-----
     ttk::button .tabs.collection.rightpane.addbutton -text "add" -command alist_gui::add_dialog
     grid .tabs.collection.rightpane.addbutton -column 0 -row 0
-    ttk::treeview .tabs.collection.rightpane.list -columns "series type genre no_eps watched" -show "headings"
+    ttk::treeview .tabs.collection.rightpane.list -columns "series no_eps watched status" -show "headings"
     .tabs.collection.rightpane.list heading series -text Series
-    .tabs.collection.rightpane.list heading type -text Type
-    .tabs.collection.rightpane.list heading genre -text Genre
     .tabs.collection.rightpane.list heading no_eps -text Episodes
     .tabs.collection.rightpane.list heading watched -text Watched
+    .tabs.collection.rightpane.list heading status -text Status
 
     grid .tabs.collection.rightpane.list -column 1 -row 0 -sticky nsew 
-
+    ::alist_gui::populate_list
 #--------Configure Grid---------------
     grid columnconfigure . 0 -weight 1
     grid .tabs -column 0 -row 0 -sticky nsew
     grid .tabs.collection.rightpane -column 0 -row 0 -padx 10 -pady 10 -sticky nsew 
     grid .tabs.collection.leftlist -column 1 -row 0 -padx 10 -pady 10
 
-    ::db_handler::add_title "generic anime" 12 0 " " " "  1 "TV" " " " " 
-    puts [::db_handler::get_anime_list]   }
+    }
 }
 
 alist_gui::start
